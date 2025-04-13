f1ed455836e16d26678a5f780d0b1bad370f285f
yuki.toyama <tune.a.piano.toyama@gmail.com>
2025-04-13 15:04:45 +0900

    画面で使う文言など追加

--- lib/common/constData.dart
+++ lib/common/constData.dart
@@ -9,4 +9,31 @@ class constData{
   static const String engineerCollection = "技術者登録";
   static const String engineerSearch = "技術者検索";
   static const String engineerDetail = "技術者詳細";
+
+  ///画面使用文言
+  /* 共通 */
+  static const String hyphen = "-";
+  static const String blank = "";
+  static const String colon = ":";
+  static const String slash= "/";
+  static const String space = " ";
+  static const String comma = ",";
+  static const String triangle= "△";//experience_categoryで1以下、years_categoryで1以下
+  static const String circle= "○";//experience_categoryで2、years_categoryで2or3以下
+  static const String doubleCircle= "◎";//experience_categoryで３、years_categoryで４以上
+  static const String experienced = "経験有";
+  static const String noExperience = "経験無";
+
+  /* 検索画面 */
+  static const String engineerSearchStation1  = "最寄駅";
+  static const String engineerSearchStation2 = "駅";
+  static const String engineerSearchCodeLanguages1  = "経験言語";
+  static const String engineerSearchProcess1  = "工程経験";
+  static const String engineerSearchDb1  = "DB経験";
+  static const String engineerSearchOs1  = "OS経験";
+  static const String engineerSearchCloud1  = "クラウド技術";
+  static const String engineerSearchTool1  = "ツール経験";
+
+
+
 }
